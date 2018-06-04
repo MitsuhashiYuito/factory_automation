a8d4147bcf6371d2fa031e8188f1767812e90eaf
msrks <mshrrks@gmail.com>
2018-06-04 16:42:49 +0900

    second commit

--- _demo.py
+++ _demo.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import time
 import subprocess
+import time
 
 #W, H = 480, 640
 #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, H)
@@ -24,26 +25,30 @@ while(True):
     roi = frame_canny[CROP_H:-CROP_H, CROP_W:-CROP_W]
 
     num_white = cv2.countNonZero(roi)
-    is_exist = True if num_white > 800 else False
+    is_exist = True if num_white > 700 else False
 
     if is_exist:
         # find moments
         print(num_white)
         mu = cv2.moments(roi, False)
         x, y = int(mu["m10"]/mu["m00"])+CROP_W, int(mu["m01"]/mu["m00"])+CROP_H
-        frame = cv2.circle(frame, (x, y), 20, (0, 0, 255), -1)
+ #       frame = cv2.circle(frame, (x, y), 20, (0, 0, 255), -1)
 
     output_image = np.zeros((W*2, H*2, 3))
     output_image[:W, :H] = frame/255
     output_image[W:, :H] = np.stack((mask, mask, mask), axis=-1)
     output_image[:W, H:] = np.stack((frame_canny, frame_canny, frame_canny), axis=-1)
     output_image[W+CROP_H:-CROP_H, H+CROP_W:-CROP_W] = np.stack((roi, roi, roi), axis=-1)
+
     cv2.imshow('demo', output_image)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     if is_exist:
-        subprocess.call(["python3", "_pick_place.py"])
+#        subprocess.call(["python3", "_pick_place.py"])
+        now = time.time()
+        cv2.imwrite('sd_0/' + str(now) +  '.png',frame)
+        print('now capture', now)
         break
         
 cap.release()
--- demo_master.py
+++ demo_master.py
@@ -5,4 +5,4 @@ subprocess.call(["python3", "_pick_place.py"])
 
 while True:
     subprocess.call(["python3", "_demo.py"])
-    time.sleep(1)
+    time.sleep(.001)
